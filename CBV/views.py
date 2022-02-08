b91985f0461f839de0f18aa7cbefc922cc10c027
taher <tasd871@gmail.com>
2022-02-08 22:26:49 +0330

    CBV & template view

---
+++ CBV/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import Post
+
+
+# Create your views here.
+class HomePage(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.get(id=1)
+        return context

561645a1ef50c46dc98524e426e9de87e511caf9
taher <tasd871@gmail.com>
2022-02-12 00:02:42 +0330

    list view for book model

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
-from books.views import BookDetailView, IndexView
+from books.views import BookDetailView, IndexView, GenreView
 app_name = 'books'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('g/<str:genre>', GenreView.as_view(), name='genre'),
     path('<slug:slug>/', BookDetailView.as_view(), name='book-detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -3,19 +3,41 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.utils import timezone
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.detail import DetailView
 
 from books.models import Book
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
+    template_name = "book/home.html"
+    model = Book
+    context_object_name = 'books'
+    # paginate our objects
+    paginate_by = 4
+
+    # with that it will show just two object from db
+    '''def get_queryset(self):
+        return Book.objects.all()[:2]'''
+
+
+class GenreView(ListView):
+    template_name = "book/home.html"
+    model = Book
+    context_object_name = 'books'
+    paginate_by = 2
+
+    def get_queryset(self, *args, **kwargs):
+        return Book.objects.filter(genre__contains=self.kwargs.get('genre'))
+
+
+'''class IndexView(TemplateView):
     template_name = "book/home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['books'] = Book.objects.all()
-        return context
+        return context'''
 
 
 class BookDetailView(DetailView):
